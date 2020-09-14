c10e1a00f2ec0f0cda912c851e2c1ce8b682ba9c
Herp Derpinstine <herpderpinstine@gmail.com>
2020-09-14 06:25:08 -0500

    first commit

---
+++ Main.cs
@@ -0,0 +1,67 @@
+﻿using MelonLoader;
+
+namespace TestMod
+{
+    public static class BuildInfo
+    {
+        public const string Name = "TestMod"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Mod for Testing"; // Description for the Mod.  (Set as null if none)
+        public const string Author = "TestAuthor"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class TestMod : MelonMod
+    {
+        public override void OnApplicationStart() // Runs after Game Initialization.
+        {
+            MelonLogger.Msg("OnApplicationStart");
+        }
+
+        public override void OnSceneWasLoaded(int buildindex) // Runs when a Scene has Loaded.
+        {
+            MelonLogger.Msg("OnSceneWasLoaded: " + buildindex.ToString());
+        }
+
+        public override void OnSceneWasInitialized(int buildindex) // Runs when a Scene has Initialized.
+        {
+            MelonLogger.Msg("OnSceneWasInitialized: " + buildindex.ToString());
+        }
+
+        public override void OnUpdate() // Runs once per frame.
+        {
+            MelonLogger.Msg("OnUpdate");
+        }
+
+        public override void OnFixedUpdate() // Can run multiple times per frame. Mostly used for Physics.
+        {
+            MelonLogger.Msg("OnFixedUpdate");
+        }
+
+        public override void OnLateUpdate() // Runs once per frame after OnUpdate and OnFixedUpdate have finished.
+        {
+            MelonLogger.Msg("OnLateUpdate");
+        }
+
+        public override void OnGUI() // Can run multiple times per frame. Mostly used for Unity's IMGUI.
+        {
+            MelonLogger.Msg("OnGUI");
+        }
+
+        public override void OnApplicationQuit() // Runs when the Game is told to Close.
+        {
+            MelonLogger.Msg("OnApplicationQuit");
+        }
+
+        public override void OnPreferencesApplied() // Runs when Melon Preferences get saved.
+        {
+            MelonLogger.Msg("OnPreferencesApplied");
+        }
+
+        public override void VRChat_OnUiManagerInit() // Runs upon VRChat's UiManager Initialization. Only runs if the Melon is used in VRChat.
+        {
+            MelonLogger.Msg("VRChat_OnUiManagerInit");
+        }
+    }
+}
